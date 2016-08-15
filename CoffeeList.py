5ff57e94e96fe8e25981df258f1a072cb3c35757
duscheln <lennart-duschek@web.de>
2016-08-15 23:02:32 +0200

    Added a flask-admin site to the page to add and delete users/items. Additionally the admin page is secured with a flask-login module

--- CoffeeList.py
+++ CoffeeList.py
@@ -1,5 +1,10 @@
+import os
 from flask import Flask, redirect,url_for,render_template,request
 from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin,expose,helpers,AdminIndexView
+from flask_admin.contrib.sqla import ModelView
+import flask_login as loginflask
+from wtforms import form, fields, validators
 from datetime import datetime
 
 
@@ -17,7 +22,13 @@ url = url.format(user, password, host, port, db)
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SECRET_KEY'] = '123456790'
+
+
 db = SQLAlchemy(app)
+
+
+
 
 class history(db.Model):
     historyID = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -72,6 +83,103 @@ class item(db.Model):
     def __repr__(self):
         return '<Item %r>' % self.name
 
+
+class coffeeadmin(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(64))
+
+    # Flask-Login integration
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.id
+
+
+class LoginForm(form.Form):
+    login = fields.StringField(validators=[validators.required()])
+    password = fields.PasswordField(validators=[validators.required()])
+
+    def validate_login(self, field):
+        user = self.get_user()
+
+        if user is None:
+            raise validators.ValidationError('Invalid user')
+
+        # we're comparing the plaintext pw with the the hash from the db
+        if user.password != self.password.data:
+        # to compare plain text passwords use
+        # if user.password != self.password.data:
+            raise validators.ValidationError('Invalid password')
+
+    def get_user(self):
+        return db.session.query(coffeeadmin).filter_by(name=self.login.data).first()
+
+class RegistrationForm(form.Form):
+    login = fields.StringField(validators=[validators.required()])
+    email = fields.StringField()
+    password = fields.PasswordField(validators=[validators.required()])
+
+    def validate_login(self, field):
+        if db.session.query(coffeeadmin).filter_by(name=self.login.data).count() > 0:
+            raise validators.ValidationError('Duplicate username')
+
+def init_login():
+    login_manager = loginflask.LoginManager()
+    login_manager.init_app(app)
+
+    # Create user loader function
+    @login_manager.user_loader
+    def load_user(user_id):
+        return db.session.query(coffeeadmin).get(user_id)
+
+class MyModelView(ModelView):
+
+    def is_accessible(self):
+        return loginflask.current_user.is_authenticated
+
+class MyAdminIndexView(AdminIndexView):
+
+    @expose('/')
+    def index(self):
+        if not loginflask.current_user.is_authenticated:
+            return redirect(url_for('.login_view'))
+        return super(MyAdminIndexView, self).index()
+
+    @expose('/login/', methods=('GET', 'POST'))
+    def login_view(self):
+        # handle user login
+        form = LoginForm(request.form)
+        if helpers.validate_form_on_submit(form):
+            user = form.get_user()
+            loginflask.login_user(user)
+
+        if loginflask.current_user.is_authenticated:
+            return redirect(url_for('.index'))
+        self._template_args['form'] = form
+#        self._template_args['link'] = link
+        return super(MyAdminIndexView, self).index()
+
+    @expose('/logout/')
+    def logout_view(self):
+        loginflask.logout_user()
+        return redirect(url_for('.index'))
+
+init_login()
+admin = Admin(app, name = 'CoffeeList Admin Page',index_view=MyAdminIndexView(), base_template='my_master.html')
+admin.add_view(MyModelView(history, db.session))
+admin.add_view(MyModelView(user, db.session))
+admin.add_view(MyModelView(item, db.session))
+
+
 @app.route('/')
 def hello():
     users = list()
@@ -81,6 +189,7 @@ def hello():
                       'id': '{}'.format(instance.userid)})
 
     return render_template('index.html', users=users)
+
 
 @app.route('/login/<int:userid>',methods = ['GET'])
 def login(userid):
@@ -121,7 +230,43 @@ def change(userid):
 
     return redirect(url_for('login',userid=userid))
 
+def build_sample_db():
+
+    db.drop_all()
+    db.create_all()
+
+    name = [
+        'Wilhelm Kackebart', 'Franz Powischer', 'Berta Haufen', 'Fritz Sabbert']
+    email = [
+        'wilhelm@kackebart.de', 'franz@powischer.de', 'berta@haufen.de', 'fritz@sabbert.de']
+
+    for i in range(len(name)):
+        user = user()
+        user.username = name[i]
+        user.email = email[i]
+        db.session.add(user)
+
+    itemname = ['coffe','water','snacks','cola']
+    price   = [0.5,0.9,0.6,0.3]
+
+    for i in range(len(name)):
+        item = item()
+        item.name = itemname[i]
+        item.price = price[i]
+        db.session.add(item)
+
+
+    db.session.commit()
+    return
+
+
 if __name__ == "__main__":
-    app.run()
+    app_dir = os.path.realpath(os.path.dirname(__file__))
+    database_path = os.path.join(app_dir, 'sqlite/TestDB.db')
+    if not os.path.exists(database_path):
+        build_sample_db()
+
+
+    app.run(debug = True)
 
 
