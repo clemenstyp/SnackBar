d5ffd490a8f7fb315349c823b28df1179abb33a5
duscheln <lennart-duschek@web.de>
2016-09-06 23:18:27 +0200

    Added a basic email option to the code that sends the coffeelist as attachment to specific email adresses

--- collectTotalBill.py
+++ collectTotalBill.py
@@ -1,6 +1,8 @@
 from sqlalchemy import *
 from CoffeeList import makeXLSBill
 from datetime import datetime
+from sendEmail import Bimail
+import os
 
 dbuser = 'coffee'
 password = 'ilikecoffee'
@@ -17,7 +19,24 @@ connection = engine.connect()
 
 filename = 'CoffeeBill_{}_{}.xls'.format(datetime.now().date().isoformat(),
                                          datetime.now().time().strftime('%H-%M-%S'))
-fullpath = ''
+fullpath = 'static'
 
 makeXLSBill(filename,fullpath)
 
+import credentials
+# subject and recipients
+mymail = Bimail('CoffeList for the ' + datetime.now().strftime('%Y/%m/%d'), ['recipient@provider.com'])
+mymail.sendername = 'Nameofthesender@provider.com'
+mymail.sender = credentials.username
+mymail.senderpass = credentials.password
+mymail.servername = 'name.of.the.smtp.server:587'
+
+# start html body. Here we add a greeting.
+mymail.htmladd('Godd Morning<br>You can find detailed version of the current coffeelist bill in the attachement.<br>')
+# Further things added to body are separated by a paragraph, so you do not need to worry about newlines for new sentences
+# here we add a line of text and an html table previously stored in the variable
+# add image chart title
+# attach another file
+mymail.addattach([os.path.join(fullpath, filename)])
+# send!
+mymail.send()
