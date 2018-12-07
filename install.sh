9471d04ff20573f15af0f4db598d3abd08fd1c35
Clemens Putschli <clemens@putschli.de>
2018-12-07 10:10:41 +0100

    reverted some changes and updated install script

--- install.sh
+++ install.sh
@@ -1,5 +1,55 @@
 #!/bin/bash
 
-ln -s /usr/local/bin/SnackBar/snackbar.service /lib/systemd/system/snackbar.service
-systemctl enable snackbar.service
+#!/bin/bash
+# Installation Script
+#
+if [[ $(whoami) != 'root' ]]; then
+  echo ""
+  echo "please run this script as root!"
+  exit
+fi
+
+echo "--- Install requirements ---"
+echo ""
+apt-get update
+apt-get install -y --no-install-recommends python3-pip
+pip3 install -r requirements.txt --upgrade
+
+echo "--- Install service ---"
+echo ""
+name="snackbar"
+workingDir=`pwd`
+python3=`which python3`
+exec="${python3} ${workingDir}/SnackBar.py --port 5000 --host 0.0.0.0"
+
+cat > ${name}.service << EOF
+[Unit]
+Description=${name}
+After=multi-user.target
+
+[Service]
+Type=simple
+WorkingDirectory=${workingDir}
+ExecStart=${exec}
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+mv  ${name}.service /lib/systemd/system/${name}.service
+chmod 644 /lib/systemd/system/${name}.service
 systemctl daemon-reload
+systemctl enable ${name}.service
+
+echo "--- start service ---"
+echo ""
+systemctl start ${name}.service
+echo "--- install done ---"
+echo ""
+echo "show status with:"
+echo "sudo systemctl status ${name}.service"
+
+echo ""
+echo "show log with:"
+echo "sudo journalctl -f -u ${name}.service"
