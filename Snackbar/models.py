7f77f4eb23a878776b9a0993fc221bdaafd2ca24
Clemens Eyhoff <clemens@eyhoff.de>
2024-06-14 13:26:52 +0200

    Update models.py

--- Snackbar/models.py
+++ Snackbar/models.py
@@ -142,12 +142,12 @@ class History(db.Model):
         return self.user_placeholder
 
     #@username_or_placeholder.inplace.expression
-    #@classmethod
-    #def _username_or_placeholder_expression(cls):
-    #    return case(
-    #        (cls.user != None, select(User).where(User.userid == cls.userid)).first().username,
-    #        else_=cls.user_placeholder
-    #    )
+    @classmethod
+    def _username_or_placeholder_expression(cls):
+        return case(
+            (cls.user != None, select(User).where(User.userid == cls.userid).first().firstName + " " + select(User).where(User.userid == cls.userid).first().lastName,
+            else_=cls.user_placeholder)
+        )
 
     itemid: Mapped[int] = mapped_column(ForeignKey('item.itemid'), nullable=True)
     item: Mapped["Item"] = relationship(back_populates="history")
@@ -164,8 +164,8 @@ class History(db.Model):
     @classmethod
     def _item_or_placeholder_expression(cls):
         return case(
-            (cls.item != None, select(Item).where(Item.itemid == cls.itemid)).first().name,
-            else_=cls.item_placeholder
+            (cls.item != None, select(Item).where(Item.itemid == cls.itemid).first().name,
+            else_=cls.item_placeholder)
         )
     
     price: Mapped[float] = mapped_column(nullable=False)
@@ -195,8 +195,8 @@ class Inpayment(db.Model):
     #@classmethod
     #def _username_or_placeholder_expression(cls):
     #    return case(
-    #        (cls.user != None, select(User).where(User.userid == cls.userid)).first().username,
-    #        else_=cls.user_placeholder
+    #        (cls.user != None, select(User).where(User.userid == cls.userid).first().username,
+    #        else_=cls.user_placeholder)
     #    )
     
     amount: Mapped[float] = mapped_column(nullable=False)
